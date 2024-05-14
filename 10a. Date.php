c94d898e784c8e87f246f522b259686491f262d0
Shreyash bandekar <98264370+shreyashbandekar@users.noreply.github.com>
2024-05-14 21:05:05 +0530

    Update 10a. Date.php

--- 10a. Date.php
+++ 10a. Date.php
@@ -6,7 +6,9 @@
    <body>
       <?php
           echo "Today is ".date("d-m-Y")."<br>";
- echo "Today is ".date("l");
+            echo "Today is ".date("l")."<br>";
+            echo "Time is ".date("h:i:sa")."<br>";
+            echo "Time is ".date("h:i:sa",strtotime("+ 5 hours + 30 minutes + 45 seconds"))."<br>";
       ?>
    </body>
 </html>
